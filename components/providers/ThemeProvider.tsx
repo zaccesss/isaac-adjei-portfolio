832d5a7128ff249128e22b4e7da6da5b20edf66f
Isaac Adjei <offices.isaac@gmail.com>
2026-05-02 14:34:51 +0100

    fix: update next-themes type import and bump puppeteer to 24.42.0

--- components/providers/ThemeProvider.tsx
+++ components/providers/ThemeProvider.tsx
@@ -5,8 +5,7 @@
 // All the configuration props (attribute, defaultTheme, etc.) are passed through from app/layout.tsx.
 
 import * as React from "react"
-import { ThemeProvider as NextThemesProvider } from "next-themes"
-import { type ThemeProviderProps } from "next-themes/dist/types"
+import { ThemeProvider as NextThemesProvider, type ThemeProviderProps } from "next-themes"
 
 export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
   return <NextThemesProvider {...props}>{children}</NextThemesProvider>
