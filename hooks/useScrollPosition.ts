5ec240a1689cf8c19bd3da797c8866a4a1650c7e
Isaac Adjei <offices.isaac@gmail.com>
2026-06-18 17:47:09 +0100

    fix: call handleScroll() for initial read to satisfy react-hooks/set-state-in-effect

--- hooks/useScrollPosition.ts
+++ hooks/useScrollPosition.ts
@@ -19,7 +19,7 @@ export function useScrollPosition() {
       cancelAnimationFrame(rafId)
       rafId = requestAnimationFrame(() => setScrollY(window.scrollY))
     }
-    setScrollY(window.scrollY)
+    handleScroll()
     window.addEventListener("scroll", handleScroll, { passive: true })
     return () => {
       window.removeEventListener("scroll", handleScroll)
