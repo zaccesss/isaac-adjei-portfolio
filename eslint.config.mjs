7957ac286e901274baf7f5c0764c10b271b840bf
Isaac Adjei <offices.isaac@gmail.com>
2026-06-18 23:09:10 +0100

    chore: release v2.9.0

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,7 +3,7 @@ import nextVitals from "eslint-config-next/core-web-vitals";
 const config = [
   ...nextVitals,
   {
-    ignores: [".next/**", "out/**", "build/**", "next-env.d.ts"],
+    ignores: [".next/**", "out/**", "build/**", "next-env.d.ts", ".open-next/**", ".wrangler/**"],
   },
 ];
 
